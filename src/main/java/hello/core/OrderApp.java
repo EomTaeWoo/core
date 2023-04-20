997c8231498c5ed444d21d0f83aa4f43bfbd5d7a
EomTaeWoo <umtanggg@naver.com>
2023-04-21 00:47:56 +0900

    핵심원리 객체 지향 원리

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,11 +7,23 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImple;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImple();
+
+        //AppConfig appConfig = new AppConfig();
+        //MemberService memberService = appConfig.memberService();
+        //OrderService orderService = appConfig.orderService();
+
+        //MemberService memberService = new MemberServiceImpl();
+        //OrderService orderService = new OrderServiceImple();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
--- src/main/java/hello/core/order/OrderServiceImple.java
+++ src/main/java/hello/core/order/OrderServiceImple.java
@@ -2,14 +2,23 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPoicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImple implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPoicy discountPoicy = new FixDiscountPolicy();
+    private final MemberRepository memberRepository;
+    private final DiscountPoicy discountPoicy;
+    //private final DiscountPoicy discountPoicy = new FixDiscountPolicy();
+    //private final DiscountPoicy discountPoicy = new RateDiscountPolicy();
+    //private DiscountPoicy discountPoicy;    // 인터페이스에만 의존한다.
+
+    public OrderServiceImple(MemberRepository memberRepository, DiscountPoicy discountPoicy) {
+        this.memberRepository = memberRepository;
+        this.discountPoicy = discountPoicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
